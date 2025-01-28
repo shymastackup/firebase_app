9ef607027c41a4915410db45a64b11a38189a15b
shyma <shyma.stackup@gmail.com>
2025-01-28 12:57:50 +0530

    first commit

--- lib/homepge.dart
+++ lib/homepge.dart
@@ -1,62 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'login_page.dart';
 
-// class HomePage extends StatelessWidget {
-//   const HomePage({super.key});
-
-//   Future<void> signOut(BuildContext context) async {
-//     await FirebaseAuth.instance.signOut();
-//     Navigator.pushReplacement(
-//       context,
-//       MaterialPageRoute(builder: (context) => LoginPage()),
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Home Page'),
-//         backgroundColor: Colors.blueAccent,
-//         actions: [
-//           IconButton(
-//             icon: const Icon(Icons.exit_to_app),
-//             onPressed: () => signOut(context),
-//           ),
-//         ],
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'Welcome to the Home Page!',
-//               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-//             ),
-//             const SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () {
-//                 // Implement navigation to another page
-//               },
-//               style: ElevatedButton.styleFrom(
-//                 backgroundColor: Colors.blueAccent,
-//                 padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(12),
-//                 ),
-//               ),
-//               child: const Text(
-//                 'Go to another page',
-//                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 
 import 'package:flutter/material.dart';
@@ -69,11 +11,10 @@ class HomePage extends StatelessWidget {
 
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  // Function to handle Google sign-out
   Future<void> signOut(BuildContext context) async {
     try {
-      await FirebaseAuth.instance.signOut(); // Sign out from Firebase
-      await _googleSignIn.signOut(); // Sign out from Google
+      await FirebaseAuth.instance.signOut(); 
+      await _googleSignIn.signOut(); 
 
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Logged out from Google!')),
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,238 +1,3 @@
-
-
-// import 'package:backend_project/signup_page.dart';
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-
-// class LoginPage extends StatelessWidget {
-//   LoginPage({super.key});
-
-//   final TextEditingController emailController = TextEditingController();
-//   final TextEditingController passwordController = TextEditingController();
-//   final _formKey = GlobalKey<FormState>();
-
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-//   final GoogleSignIn _googleSignIn = GoogleSignIn();
-
-//   Future<void> signInWithGoogle(BuildContext context) async {
-//     try {
-//       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-//       if (googleUser == null) {
-//         return;
-//       }
-
-//       final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-
-//       final AuthCredential credential = GoogleAuthProvider.credential(
-//         accessToken: googleAuth.accessToken,
-//         idToken: googleAuth.idToken,
-//       );
-
-//       await _auth.signInWithCredential(credential);
-
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         const SnackBar(content: Text('Logged in with Google!')),
-//       );
-//       Navigator.pushReplacementNamed(context, '/home');
-//     } catch (e) {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content: Text('Google Sign-in failed: ${e.toString()}')),
-//       );
-//     }
-//   }
-
-//   Future<void> login(BuildContext context) async {
-//     if (_formKey.currentState!.validate()) {
-//       try {
-//         await FirebaseAuth.instance.signInWithEmailAndPassword(
-//           email: emailController.text.trim(),
-//           password: passwordController.text.trim(),
-//         );
-//         Navigator.pushReplacementNamed(context, '/home');
-//       } catch (e) {
-//         ScaffoldMessenger.of(context).showSnackBar(
-//           SnackBar(content: Text('Login failed: ${e.toString()}')),
-//         );
-//       }
-//     }
-//   }
-
-//   Future<void> resetPassword(BuildContext context) async {
-//     if (emailController.text.isNotEmpty &&
-//         RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").hasMatch(emailController.text.trim())) {
-//       try {
-//         await FirebaseAuth.instance.sendPasswordResetEmail(email: emailController.text.trim());
-//         ScaffoldMessenger.of(context).showSnackBar(
-//           const SnackBar(content: Text('Password reset email sent!')),
-//         );
-//       } catch (e) {
-//         ScaffoldMessenger.of(context).showSnackBar(
-//           SnackBar(content: Text('Error: ${e.toString()}')),
-//         );
-//       }
-//     } else {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         const SnackBar(content: Text('Enter a valid email to reset password')),
-//       );
-//     }
-//   }
-
-//   // Function to handle Google sign-out
-//   Future<void> signOutFromGoogle(BuildContext context) async {
-//     try {
-//       await _auth.signOut(); // Sign out from Firebase
-//       await _googleSignIn.signOut(); // Sign out from Google
-
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         const SnackBar(content: Text('Logged out from Google!')),
-//       );
-//       Navigator.pushReplacementNamed(context, '/login'); // Navigate to login page or home
-//     } catch (e) {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content: Text('Error logging out: ${e.toString()}')),
-//       );
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Login', style: TextStyle(fontWeight: FontWeight.bold)),
-//         backgroundColor: Colors.blueAccent,
-//       ),
-//       body: SingleChildScrollView(
-//         child: Padding(
-//           padding: const EdgeInsets.all(20.0),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: <Widget>[
-//               const SizedBox(height: 30),
-//               const Text(
-//                 'Login to Your Account',
-//                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blueAccent),
-//               ),
-//               const SizedBox(height: 20),
-//               Form(
-//                 key: _formKey,
-//                 child: Column(
-//                   children: [
-//                     TextFormField(
-//                       controller: emailController,
-//                       decoration: InputDecoration(
-//                         labelText: 'Email',
-//                         hintText: 'Enter your email',
-//                         border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-//                         contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
-//                         filled: true,
-//                         fillColor: Colors.blue[50],
-//                       ),
-//                       validator: (value) {
-//                         if (value == null || value.isEmpty) {
-//                           return 'Please enter your email';
-//                         } else if (!RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").hasMatch(value)) {
-//                           return 'Enter a valid email';
-//                         }
-//                         return null;
-//                       },
-//                     ),
-//                     const SizedBox(height: 20),
-//                     StatefulBuilder(
-//                       builder: (context, setState) {
-//                         bool _obscurePassword = true;
-//                         return TextFormField(
-//                           controller: passwordController,
-//                           obscureText: _obscurePassword,
-//                           decoration: InputDecoration(
-//                             labelText: 'Password',
-//                             hintText: 'Enter your password',
-//                             border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-//                             contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
-//                             filled: true,
-//                             fillColor: Colors.blue[50],
-//                             suffixIcon: IconButton(
-//                               icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility),
-//                               onPressed: () {
-//                                 setState(() {
-//                                   _obscurePassword = !_obscurePassword;
-//                                 });
-//                               },
-//                             ),
-//                           ),
-//                           validator: (value) {
-//                             if (value == null || value.isEmpty) {
-//                               return 'Please enter your password';
-//                             } else if (value.length < 6) {
-//                               return 'Password must be at least 6 characters';
-//                             }
-//                             return null;
-//                           },
-//                         );
-//                       },
-//                     ),
-//                     const SizedBox(height: 10),
-
-//                     Align(
-//                       alignment: Alignment.centerRight,
-//                       child: TextButton(
-//                         onPressed: () => resetPassword(context),
-//                         child: const Text('Forgot Password?', style: TextStyle(color: Colors.blueAccent)),
-//                       ),
-//                     ),
-//                     const SizedBox(height: 10),
-
-//                     ElevatedButton(
-//                       onPressed: () => login(context),
-//                       style: ElevatedButton.styleFrom(
-//                         backgroundColor: Colors.blueAccent,
-//                         padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-//                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-//                       ),
-//                       child: const Text('Login', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-//                     ),
-//                     const SizedBox(height: 20),
-
-//                     ElevatedButton(
-//                       onPressed: () => signInWithGoogle(context),
-//                       style: ElevatedButton.styleFrom(
-//                         backgroundColor: const Color.fromARGB(255, 215, 170, 170),
-//                         padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-//                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-//                       ),
-//                       child: const Text('Login with Google', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-//                     ),
-//                     const SizedBox(height: 20),
-
-//                     TextButton(
-//                       onPressed: () {
-//                         Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
-//                       },
-//                       child: const Text('Don\'t have an account? Sign Up', style: TextStyle(color: Colors.blueAccent)),
-//                     ),
-                    
-//                     const SizedBox(height: 20),
-//                     ElevatedButton(
-//                       onPressed: () => signOutFromGoogle(context),
-//                       style: ElevatedButton.styleFrom(
-//                         backgroundColor: Colors.redAccent,
-//                         padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-//                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-//                       ),
-//                       child: const Text('Logout from Google', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
 import 'package:backend_project/signup_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -255,7 +20,8 @@ class LoginPage extends StatelessWidget {
         return;
       }
 
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
 
       final AuthCredential credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
@@ -293,9 +59,11 @@ class LoginPage extends StatelessWidget {
 
   Future<void> resetPassword(BuildContext context) async {
     if (emailController.text.isNotEmpty &&
-        RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").hasMatch(emailController.text.trim())) {
+        RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+            .hasMatch(emailController.text.trim())) {
       try {
-        await FirebaseAuth.instance.sendPasswordResetEmail(email: emailController.text.trim());
+        await FirebaseAuth.instance
+            .sendPasswordResetEmail(email: emailController.text.trim());
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Password reset email sent!')),
         );
@@ -315,7 +83,8 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login', style: TextStyle(fontWeight: FontWeight.bold)),
+        title:
+            const Text('Login', style: TextStyle(fontWeight: FontWeight.bold)),
         backgroundColor: Colors.blueAccent,
       ),
       body: SingleChildScrollView(
@@ -327,7 +96,10 @@ class LoginPage extends StatelessWidget {
               const SizedBox(height: 30),
               const Text(
                 'Login to Your Account',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blueAccent),
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent),
               ),
               const SizedBox(height: 20),
               Form(
@@ -339,15 +111,19 @@ class LoginPage extends StatelessWidget {
                       decoration: InputDecoration(
                         labelText: 'Email',
                         hintText: 'Enter your email',
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                        contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 16, horizontal: 12),
                         filled: true,
                         fillColor: Colors.blue[50],
                       ),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
                           return 'Please enter your email';
-                        } else if (!RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").hasMatch(value)) {
+                        } else if (!RegExp(
+                                r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+                            .hasMatch(value)) {
                           return 'Enter a valid email';
                         }
                         return null;
@@ -363,13 +139,18 @@ class LoginPage extends StatelessWidget {
                           decoration: InputDecoration(
                             labelText: 'Password',
                             hintText: 'Enter your password',
-                            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                            contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12)),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 16, horizontal: 12),
                             filled: true,
                             fillColor: Colors.blue[50],
                             suffixIcon: IconButton(
                               // ignore: dead_code
-                              icon: Icon(_obscurePassword ? Icons.visibility_off : Icons.visibility),
+                              icon: Icon(_obscurePassword
+                                  ? Icons.visibility_off
+                                  // ignore: dead_code
+                                  : Icons.visibility),
                               onPressed: () {
                                 setState(() {
                                   _obscurePassword = !_obscurePassword;
@@ -389,43 +170,53 @@ class LoginPage extends StatelessWidget {
                       },
                     ),
                     const SizedBox(height: 10),
-
                     Align(
                       alignment: Alignment.centerRight,
                       child: TextButton(
                         onPressed: () => resetPassword(context),
-                        child: const Text('Forgot Password?', style: TextStyle(color: Colors.blueAccent)),
+                        child: const Text('Forgot Password?',
+                            style: TextStyle(color: Colors.blueAccent)),
                       ),
                     ),
                     const SizedBox(height: 10),
-
                     ElevatedButton(
                       onPressed: () => login(context),
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blueAccent,
-                        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)),
                       ),
-                      child: const Text('Login', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                      child: const Text('Login',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
                     ),
                     const SizedBox(height: 20),
-
                     ElevatedButton(
                       onPressed: () => signInWithGoogle(context),
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 215, 170, 170),
-                        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                        backgroundColor:
+                            const Color.fromARGB(255, 215, 170, 170),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)),
                       ),
-                      child: const Text('Login with Google', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                      child: const Text('Login with Google',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
                     ),
                     const SizedBox(height: 20),
-
                     TextButton(
                       onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignUpPage()));
                       },
-                      child: const Text('Don\'t have an account? Sign Up', style: TextStyle(color: Colors.blueAccent)),
+                      child: const Text('Don\'t have an account? Sign Up',
+                          style: TextStyle(color: Colors.blueAccent)),
                     ),
                   ],
                 ),
@@ -437,8 +228,3 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
